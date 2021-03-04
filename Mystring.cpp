7de00d4f746fd461689fddcfe73b6e5177bcfb8a
BrianLevin <bml201095@gmail.com>
2021-03-04 11:03:42 -0500

    created the overloaded constructor  for the my string class

--- Mystring.cpp
+++ Mystring.cpp
@@ -9,3 +9,15 @@ Mystring::Mystring()
     str = new char[1];
     *str = '\0';
 }
+
+// Overloaded constructor
+Mystring::Mystring(const char *s) 
+    : str {nullptr} {
+        if (s==nullptr) {
+            str = new char[1];
+            *str = '\0';
+        } else {
+            str = new char[strlen(s)+1];
+            strcpy(str, s);
+        }
+}
